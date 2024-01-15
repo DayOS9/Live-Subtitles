f2ace167a944da23134762ad4f43a756255cbb85
DayOS9 <79074357+DayOS9@users.noreply.github.com>
2024-01-14 17:57:56 -0700

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,2 @@
+TOKEN = "your bot token"
+translateToken = "your google translate api key"
---
+++ languages.py
@@ -0,0 +1,6 @@
+from discord import OptionChoice
+languages = [
+		OptionChoice(name="english", value="en"),
+		OptionChoice(name="spanish", value="es"),
+		OptionChoice(name="Afrikaans", value="af")
+		]
